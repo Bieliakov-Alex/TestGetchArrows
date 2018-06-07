ab580fc349c8a744419612e39d932578928822e3
Alex Bieliakov <bieliakov.alex@gmail.com>
2018-06-08 00:06:44 +0300

    Эксперимент по использованию _getch() с клавишами стрелок. Если вводить "стрелку", _getch() вернет два значения подряд.

---
+++ TestGetchArrows/main.cpp
@@ -0,0 +1,14 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k1 = _getch();
+	int k2 = _getch();
+	int k3 = _getch();
+	cout << k1 << endl << k2 << endl << k3 << endl;
+	_getch();
+	return 0;
+}
